43da1d87866231d8794ffcd3bb2e693a6512ffd8
Leaf <85034090+oLeafDev@users.noreply.github.com>
2022-05-11 18:30:38 -0400

    uploaded files
    prt 2

---
+++ src/main/java/wynn/modules/Modules.java
@@ -0,0 +1,18 @@
+package wynn.modules;
+
+public class Modules {
+    
+    public static boolean Fly = false;
+    public static boolean Logo = false;
+    public static boolean Watermark = false;
+    public static boolean LootRun = false;
+    public static boolean ClickGUI = false;
+    public static boolean TriggerBot = false;
+    public static boolean MageAura = false;
+    public static boolean ArcherBot = false;
+    public static boolean AutoStreamer = false;
+    public static boolean NickHider = false;
+    public static boolean ChestStealer= false;
+
+    
+}
---
+++ src/main/java/wynn/modules/combat/TriggerBot.java
@@ -0,0 +1,51 @@
+package wynn.modules.combat;
+
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+import wynn.modules.Module;
+import wynn.modules.Modules;
+import wynn.ui.base.base.IToggleable;
+
+public class TriggerBot extends Module {
+
+    private static TriggerBot instance;
+    public static Minecraft mc = Minecraft.getMinecraft();
+
+    public TriggerBot() {
+        super("TriggerBot", "Does Not Work", ()->true, false);
+        instance = this;
+
+    }
+
+    public static IToggleable getToggle() {
+        return instance.isEnabled();
+    }
+
+
+    @SubscribeEvent
+    public void onTick(TickEvent.ClientTickEvent event) {
+        if (!getToggle().getAsBoolean()) {
+            return;
+        }
+        if (event.phase == TickEvent.Phase.START) {
+            if (mc.player != null) {
+                if(mc.world != null) {
+                    if (mc.objectMouseOver != null) {
+                        if (mc.objectMouseOver.entityHit != null) {
+                            if (mc.objectMouseOver.entityHit instanceof net.minecraft.entity.player.EntityPlayer) {
+                                if (!mc.player.getName().equals(mc.objectMouseOver.entityHit.getName())) {
+                                    if (mc.player.getDistance(mc.objectMouseOver.entityHit) < 5) {
+                                        mc.player.attackTargetEntityWithCurrentItem(mc.objectMouseOver.entityHit);
+                                    }
+
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
